1237ea517899d0f90c57e844522f2866aca42f9e
AuroraLopez <alopfue679@g.educaand.es>
2025-09-23 12:27:52 +0200

    Mi primer programa Java

---
+++ Prueba.java
@@ -0,0 +1,5 @@
+public class Prueba{ //Despues de class hay que poner el nombre del archivo si o si
+    public static void main(String[] args) { // Sin esto no se ve nada
+        System.out.println("Hola Aurora!!");
+    }
+}
