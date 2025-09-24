2387fbb78dd278821f4b7ea1d1b006c2b26d0bad
AuroraLopez <alopfue679@g.educaand.es>
2025-09-24 14:19:49 +0200

    Explicacion de variables y comentarios

--- fundamentos JAVA/src/App.java
+++ fundamentos JAVA/src/App.java
@@ -1,5 +1,38 @@
 public class App {
+    // public static boolean logico=true; // al estar en el ámbito app todas las funciones pueden ver esta variable es una varible global
+    // aqui arriba solo se pone las globales y constantes(mayusculas) y para poder llamarlas en alguna función hay que ponerle static
+    // public static void hacernada(){
+    //     int altura=2;
+    //     int diametro=5;
+    //     System.out.println("El area de un cuadrado es: " +(altura*diametro));
+    // }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        // char c=98;
+        // System.out.println(c); //tabla ASCII
+        // int a='a';
+        // System.out.println(a);
+        // final double pi=3.14152050;
+        // pi= 3.8374783283; // no va
+        // boolean logico=true; //false
+        // System.out.println(logico);
+        // int a=(int) 2.6; // convertir algo grande en solo entero sin rendondear
+        // double a1=(int) 2.6; // te pone un 2.0 porque es un numero real aunque tu le digas entero
+        // System.out.println(a1);
+        // int prueba=(1+2+3)/3;
+        // double prueba2=(1+2+3)/3.0;
+        // System.out.println(prueba);
+        // System.out.println(prueba2);
+        // String nombre="me llamo Aurora";
+        // System.out.println(nombre);
+        // int edad=40;
+        // System.out.println(edad);
+        // hacernada();
+        // (TIPO) IDENTIFICADOR = VALOR
+        // El primer elemento de una variable nunca puede ser un numero y evitar el $
     }
+    // diferentes ambitos = diferentes funciones si se pueden llamar igual las variables
+    // public static void funcion(){
+    //     int logico=3;
+    // }
 }
