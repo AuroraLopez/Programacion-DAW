f84c6b0dbca4791fc81c7e69bf5460b248ee6c38
AuroraLopez <alopfue679@g.educaand.es>
2025-10-14 12:26:38 +0200

    Comienzo de arrays

---
+++ ArraysJava/src/Arrays.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Arrays {
+    public static void imprimeArray(double[] array){
+        System.out.println(array[1]);
+    }
+    public static double devolverArray(double[] a){
+        return a[1]=(a[1]*3);
+    }
+    public static void main(String[] args) throws Exception {
+        //Array estático
+        double calificaciones[]={7,8,2,4,8,9};
+
+        // Array dinámico
+        double[] calificaciones2;
+        System.out.println("Indica el tamaño del aray calificaciones2: ");
+        Scanner teclado= new Scanner(System.in);
+        calificaciones2=new double[teclado.nextInt()];
+        System.out.println(calificaciones2);
+
+
+        //Array que apunta al mismo sitio
+        double calificacionesOtro[];
+        calificacionesOtro =calificaciones2;
+        System.out.println(calificacionesOtro[3]);
+        System.out.println(calificaciones2[3]);
+
+        double resultado=devolverArray(calificaciones);
+        // System.out.println(resultado[1]);
+
+        //Matrices
+        String[][] comida={{"arroz","manzana","conejo"},{"verdura","pollo","fresa"},{"patatas", "pera", "lentejas"}};
+        System.out.println(comida[0][2]);
+        String[] aux=comida[1];
+        System.out.println("Matriz "+comida[1][2]);
+        System.out.println("Vector "+aux[2]);
+
+
+        //Usar la memoria dinámica reservada
+        calificaciones2[3]=5;
+        System.out.println(calificaciones2[3]);
+        System.out.println(calificaciones2[1]);
+    }    
+}
