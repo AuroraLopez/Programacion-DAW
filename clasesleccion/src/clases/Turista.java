917ed1e5067f67f14c764af73b4df0c81815551d
AuroraLopez <alopfue679@g.educaand.es>
2025-11-19 14:22:58 +0100

    Terminacion de ejercicios

---
+++ clasesleccion/src/clases/Turista.java
@@ -0,0 +1,73 @@
+package clases;
+
+public class Turista {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String telefono;
+    private String direccion;
+
+    // Constructor
+    public  Turista() {
+    }
+
+    public Turista(int id, String nombre, String apellidos, String telefono, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    // Metodos
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+    // Imprimimos los turistas
+    public void imprimeTurista(){
+        System.out.println("Id: "+id);
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellidos: "+apellidos);
+        System.out.println("Telefono: "+telefono);
+        System.out.println("Direccion: "+direccion);
+    }
+}
