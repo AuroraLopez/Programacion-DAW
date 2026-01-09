e34f3165dab7bf4985ae07e914d04b0111c292bc
AuroraLopez <alopfue679@g.educaand.es>
2026-01-09 14:22:09 +0100

    Comienzo de uso de JSON

---
+++ Unidad5/src/Actividades/Actividad6.java
@@ -0,0 +1,33 @@
+package Actividades;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class Actividad6 {
+    public static void main(String[] args) {
+        try {
+        String superheroe;
+        String alterego;
+        // Lectura del contenido del fichero JSON
+        Object ob = new JSONParser().parse(new FileReader("C:\\Users\\\\Auri\\Desktop\\1º DAW\\Programación\\Programacion-DAW\\Unidad5\\src\\Lecciones\\heroes.json"));
+        // Convertir objeto a JSON y procesarlo
+        JSONArray arr = (JSONArray) ob;
+        // Recorrer cada elemento de ese array json
+        for (Object item : arr) {
+            JSONObject data = (JSONObject) item;
+            String publiser= (String)data.get("publisher");
+            if (publiser.contains("Marvel")) {
+                superheroe=(String)data.get("superhero");
+                alterego=(String)data.get("alter_ego");
+                System.out.println("Superheroe: "+superheroe);
+                System.out.println("AlterEgo: "+alterego);
+            }
+        }
+        } catch(Exception e){
+            System.out.println("Algo ha fallado");
+        }
+    }
+}
---
+++ Unidad5/src/Lecciones/AplicJSON.java
@@ -0,0 +1,33 @@
+package Lecciones;
+
+import java.io.FileReader;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+public class AplicJSON {
+    public static void main(String[] args) {
+        try {
+        // Lectura del contenido del fichero JSON
+        Object ob = new JSONParser().parse(new FileReader("C:\\Users\\\\Auri\\Desktop\\1º DAW\\Programación\\Programacion-DAW\\Unidad5\\src\\Lecciones\\prueba.json"));
+        // Convertir objeto a JSON y procesarlo
+        JSONObject js = (JSONObject) ob;
+        // Convirtiendo cada componente en json
+        String firstName = (String) js.get("firstName");
+        String lastName = (String) js.get("lastName");
+        System.out.println("First name is: " + firstName);
+        System.out.println("Last name is: " + lastName);
+        // Leyendo el array guardado en esa clave
+        JSONArray arr = (JSONArray) js.get("phoneNumbers");
+        // Recorrer cada elemento de ese array json
+        for (Object item : arr) {
+            JSONObject data = (JSONObject) item;
+            String post_id = (String) data.get("phone-number");
+            System.out.println(post_id);
+        }
+        } catch (Exception e) {
+            System.out.println("Algo ha fallado");
+        }
+    }
+}
